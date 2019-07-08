f5be2cd4b0807c3d62b58194c6562f4f3718154f
vizihokis <vizihokis@gmail.com>
2019-07-08 16:23:38 +0200

    init commit

---
+++ alien_invasion.py
@@ -0,0 +1,38 @@
+import sys
+
+import pygame
+
+from settings import Settings
+from ship import Ship
+
+def run_game():
+    # Initialize pygame,settings and create a screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width,ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship
+
+    ship = Ship(screen)
+
+    # Set the background color
+    bg_color = (53, 159, 220)
+    #Start the main loop for the game
+    while True:
+
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        # Redraw the screen during each pass through the loop
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+
+        #Make The most recently drawn screen visible
+        pygame.display.flip()
+
+
+run_game()
+
