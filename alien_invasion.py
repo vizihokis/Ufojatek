b79e838b6b79ed7e35eaa829cbe37be06c206a02
vizihokis <vizihokis@gmail.com>
2019-07-09 13:30:04 +0200

    golyo

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -13,22 +14,19 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # Make a ship
+    ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in
+    bullets = Group()
 
-    ship = Ship(screen)
 
-    # Set the background color
-    bg_color = (53, 159, 220)
+   
     #Start the main loop for the game
     while True:
-        gf.check_events()
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        bullets.update()
+        gf.update_screen(ai_settings,  screen, ship, bullets)
 
-        # Redraw the screen during each pass through the loop
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        #Make The most recently drawn screen visible
-        pygame.display.flip()
-
-
+  
 run_game()
 
--- settings.py
+++ settings.py
@@ -7,3 +7,11 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         
+        # Ship settings
+        self.ship_speed_factor = 1.5
+
+        # Bullet Settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
