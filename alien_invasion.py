261276c4c544532ce06af5abb6ac6ea4e5fa07b9
vizihokis <vizihokis@gmail.com>
2019-07-09 09:27:22 +0200

    egyszerusites

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,9 +1,8 @@
-import sys
-
 import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     # Initialize pygame,settings and create a screen object.
@@ -21,11 +20,8 @@ def run_game():
     bg_color = (53, 159, 220)
     #Start the main loop for the game
     while True:
+        gf.check_events()
 
-        # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
         # Redraw the screen during each pass through the loop
         screen.fill(ai_settings.bg_color)
         ship.blitme()
