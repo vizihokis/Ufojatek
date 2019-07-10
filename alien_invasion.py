d9ba17d1371821734ffb1080caa09a0d0387a2a8
vizihokis <vizihokis@gmail.com>
2019-07-10 15:18:57 +0200

    playbutton

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.sprite import Group
 from settings import Settings
+from game_stats import GameStats
+from button import Button
 from ship import Ship
 import game_functions as gf
 
@@ -12,19 +14,30 @@ def run_game():
         (ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    # Make the play button.
+    play_button = Button(ai_settings, screen, "Play")
+
+    #Create an instance to store game statistics.
+    stats = GameStats(ai_settings)
     # Make a ship
     ship = Ship(ai_settings, screen)
     # Make a group to store bullets in
     bullets = Group()
-
-
+    # Make a group of aliens.
+    aliens = Group()
+    # Make an alien fleet
+    gf.create_fleet(ai_settings, screen, ship, aliens)
    
     #Start the main loop for the game
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings,  screen, ship, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings,screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
+        gf.update_screen(ai_settings,  screen, ship, aliens, bullets, play_button)
+
 
   
 run_game()
--- settings.py
+++ settings.py
@@ -9,10 +9,17 @@ class Settings():
         
         # Ship settings
         self.ship_speed_factor = 1.5
+        self.ship_limit = 3
 
         # Bullet Settings
         self.bullet_speed_factor = 1
         self.bullet_width = 3
-        self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
-        self.bullets_allowed = 3
+        self.bullet_height = 10
+        self.bullet_color = 240, 20, 20
+        self.bullets_allowed = 5
+
+        # Alien settings
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
